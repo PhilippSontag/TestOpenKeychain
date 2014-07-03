f2e9c35be152de153f561f93bacaa37c8d001365
bennnil <bennnil-github@yahoo.com>
2014-07-03 21:36:30 +0200

    Implemented NFC

--- app/src/main/java/pSontag/testopenkeychain/WizardActivity.java
+++ app/src/main/java/pSontag/testopenkeychain/WizardActivity.java
@@ -1,13 +1,15 @@
 package pSontag.testopenkeychain;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
+import android.widget.Toast;
 
 public class WizardActivity extends FragmentActivity
-        implements SelectMethods.OnFragmentInteractionListener, Passphrase.OnFragmentInteractionListener{
+        implements SelectMethods.OnFragmentInteractionListener, Passphrase.OnFragmentInteractionListener, NFC.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,17 @@ public class WizardActivity extends FragmentActivity
         transaction.replace(R.id.fragmentContainer, passphrase).addToBackStack(null).commit();
 
     }
+
+    public void writeNFC(View view){
+
+        NFC nfc = new NFC();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentContainer, nfc).addToBackStack(null).commit();
+        //Zeig,dass du hier bist
+        Context context = getApplicationContext();
+        Toast.makeText(context, "Got here", Toast.LENGTH_SHORT).show();
+    }
+
 
     @Override
     public void onFragmentInteraction(Uri uri) {
